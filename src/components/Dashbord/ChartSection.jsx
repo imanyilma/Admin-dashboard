5eea3a074b7912e581e6f97b63457dab519d02f9
imanyilma <155858994+imanyilma@users.noreply.github.com>
2026-03-16 16:17:06 +0300

    Added new file

--- src/components/Dashbord/ChartSection.jsx
+++ src/components/Dashbord/ChartSection.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import RevnueChart from './RevnueChart'
 export default function ChartSection() {
   return (
-    <div>
-      
+    <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+      <div className="xl:col-span-2">
+         <RevnueChart />
+
+      </div>
+      <div>
+     </div>
     </div>
   )
 }
--- src/components/Dashbord/dashboard.jsx
+++ src/components/Dashbord/dashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import StatusGrid from './statusGrid'  
+import ChartSection from './ChartSection'  
 export default function Dashboard() {
   return (
     <div className="space-y-6">  
       {/* status grid */}
       <StatusGrid />  
+      {/* charts */}
+      <ChartSection />
     </div>
   )
 }
